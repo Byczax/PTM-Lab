7aed1bd6ed7ab5dd522bd32fd6545cc26baa7d36
Maciej Byczko <252747@student.pwr.edu.pl>
2021-05-12 08:13:19 +0200

    lab05 update
    Signed-off-by: Maciej Byczko <252747@student.pwr.edu.pl>

--- Lab05/alter.asm
+++ Lab05/alter.asm
@@ -1,6 +1,5 @@
 MOV R7, A       ;(1) zamiana odczytanego dziwolaga na ASCII 
 ANL 07H, #0FH   ;(2) // adres 07H to inaczej rejestr R7
-CLR C           ; wyczyszczenie C aby przypadkiem nie przeszkodzilo
-RRC A           ;(3)
-ANL A, #0F0H    ;(4)
+ANL A, #0E0H    ;(4)
+RR  A           ;(3)
 ORL A, R7       ;(5)
