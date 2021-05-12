55674b86ff764153f0260d297067d52536c027b0
qucker135 <qucker135@gmail.com>
2021-05-12 13:41:07 +0200

    Update alter.asm

--- Lab05/alter.asm
+++ Lab05/alter.asm
@@ -1,5 +1,5 @@
 MOV R7, A       ;(1) zamiana odczytanego dziwolaga na ASCII 
 ANL 07H, #0FH   ;(2) // adres 07H to inaczej rejestr R7
-ANL A, #0E0H    ;(4)
-RR  A           ;(3)
+ANL A, #0E0H    ;(3)
+RR  A           ;(4)
 ORL A, R7       ;(5)
